51eb67058f6d3af2499e642a378a74dbcd7595c3
Em <emi@10-151-216-215.vlan912.client.wireless.uic.edu>
2018-03-14 00:32:39 -0500

    ablities files

---
+++ the Barge, Atet/Assets/scripts/abilityScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityScript : MonoBehaviour {
+
+    public Solid solid;
+    public Clear clear;
+    public Heavy heavy;
+
+    public Collider s_Collider;
+    public Collider c_Collider;
+    
+    public Rigidbody Crb;
+    public Rigidbody Srb;
+    public Rigidbody Hrb;
+    
+    public Material c1_Material;
+    public Material c2_Material;
+    public Material s1_Material;
+    public Material s2_Material;
+    
+    public GameObject carriedObject;
+    public Camera mainCamera;
+    
+    
+    void start(){
+        
+        Crb = GetComponent<Rigidbody>();
+//        Crb.isKinematic = false; // Activated
+          Crb.detectCollisions = true;
+       
+        Srb = GetComponent<Rigidbody>();
+//        Srb.isKinematic = true;  // Deactivated
+        
+        Hrb = GetComponent<Rigidbody>();
+//        Hrb.isKinematic = false; // Activated
+        
+    }
+    
+    //triggers Sia' dispell-illusion ability
+        public void abilityClear(){
+            
+               //allows object to trigger an effect when within sight and range
+		Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3());
+            RaycastHit hit;
+        
+        //determines available distance
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+               
+            Clear c = hit.collider.GetComponent<Clear>();
+                
+            if (c != null){
+//                 
+//                Crb.isKinematic = true;  // Deactivated
+//                Crb.isKinematic = false; // Activated
+                
+                c2_Material = GetComponent<Renderer>().material;
+                Crb.detectCollisions = false;
+            
+//         c2_Material = GetComponent<Renderer>().material;
+                
+            } 
+        }}
+    
+    
+        //triggers Hu's create-solid ability
+        public void abilitySolid(){
+               //allows object to trigger an effect when within sight and range
+		Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3());
+            RaycastHit hit;
+        
+        //determines available distance
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+              
+                
+            Solid s = hit.collider.GetComponent<Solid>();
+            
+            if (s != null){
+                
+                Srb.isKinematic = false; // Activated
+                Srb.isKinematic = true;  // Deactivated
+                
+         s1_Material = GetComponent<Renderer>().material;
+       
+            }
+        } }
+    
+    
+    //triggers heka's heavy-lifting ability 
+        public void abilityHeavy(){
+               //allows object to trigger an effect when within sight and range
+		Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3());
+            RaycastHit hit;
+        
+        //determines available distance
+            if (Physics.Raycast(ray, out hit, 3))
+            {
+              
+            
+            Heavy h = hit.collider.GetComponent<Heavy>();
+                
+            if (h != null){
+                    Hrb.isKinematic = true;  // Deactivated
+                    Hrb.isKinematic = false; // Activated
+                }
+        }}
+}
---
+++ the Barge, Atet/Assets/scripts/abilityTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilityTrigger : MonoBehaviour {
+
+    public abilityScript ability;
+    
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+       if (Input.GetKeyDown("space")){
+            ability.abilityClear();
+                }
+                
+        else if (Input.GetKeyDown("enter")){
+            ability.abilitySolid();
+                }
+                
+         else if (Input.GetKeyDown("shift")){
+            ability.abilityHeavy();
+                }
+            }
+}
+     
+    
+                
