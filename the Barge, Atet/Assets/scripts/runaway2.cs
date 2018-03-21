219faca07c385581329e05b9f3c4c5bc8ffaa404
p-welch <welch9@uic.edu>
2018-03-21 04:12:32 -0500

    runaway2 script good to go, bugTemp

---
+++ the Barge, Atet/Assets/scripts/runaway2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runaway2 : MonoBehaviour {
+
+    // Use this for initialization
+    UnityEngine.AI.NavMeshAgent agent;
+    public int multiplier;
+    public float range;
+    public GameObject player;
+
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDirection = this.transform.position + ((this.transform.position - player.GetComponent<Transform>().position) * multiplier);
+        float distance = Vector3.Distance(this.transform.position, player.GetComponent<Transform>().position);
+
+        if (distance < range)
+            agent.SetDestination(moveDirection);
+	}
+}
---
+++ the Barge, Atet/Assets/scripts/runawayTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runawayTest : MonoBehaviour {
+
+    public Transform player;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    //public float speed;
+    public float distance;
+
+	// Use this for initialization
+	void Start ()
+    {
+            agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Run();
+	}
+
+    void Run()
+    {
+        if (player.position.y < distance)
+        {
+            Vector3 moveDirection = this.transform.position - player.transform.position;
+            agent.SetDestination(moveDirection);
+            //transform.Translate(moveDirection.normalized * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            //transform.Translate()
+        }
+
+                
+
+
+        //agent.SetDestination(runPosition);
+
+    }
+}
+
