1bc4d1212b383e9d8cd5753dd2a3ea291704dfde
Mark Haynie <mhaynie2@uic.edu>
2018-03-21 01:48:37 -0500

    added enemy
    added main enemy
    added reset button

--- the Barge, Atet/Assets/scripts/Follow.cs
+++ the Barge, Atet/Assets/scripts/Follow.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 
     UnityEngine.AI.NavMeshAgent agent;
-    public Transform target;
+    public GameObject target;
+    public float xStart = 0;
+    public float yStart = 0;
+    public float zStart = 0;
 
     void Start()
     {
@@ -14,13 +17,13 @@ public class Follow : MonoBehaviour {
 
     void Update()
     {
-        if(target.position.y < 3)
+        if(target.GetComponent<Transform>().position.x > 118)
         {
-            agent.SetDestination(target.position);
+            agent.SetDestination(target.GetComponent<Transform>().position);
         }
         else
         {
-            agent.SetDestination(new Vector3(0, 0, 0));
+            agent.SetDestination(new Vector3(xStart, yStart, zStart));
         }        
     }
 }
---
+++ the Barge, Atet/Assets/scripts/resetTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resetTrigger : MonoBehaviour {
+
+    public GameObject target;
+    public float xStart = 0;
+    public float yStart = 0;
+    public float zStart = 0;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject == target)
+        {
+            target.transform.position = new Vector3(xStart, yStart, zStart);
+        }        
+    }
+}
