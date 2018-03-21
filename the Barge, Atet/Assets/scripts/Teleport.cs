49e8a574fd8af207f3ac4c3ed8f5b116014ca9c2
Em <emi@dhcp-18-192-250.nat.wireless.uic.edu>
2018-03-21 14:13:57 -0500

    scene update
    ceilings and final walls added,
    artifact teleporter works agian

---
+++ the Barge, Atet/Assets/scripts/Teleport.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+//    public GameObject target;
+//    public GameObject spawn;
+//    
+//    public float xMove = 0;
+//    public float yMove = 0;
+//    public float zMove = 0;
+//    
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//    
+//    public void OnCollisionEnter(){
+//        
+//        if(target){
+//            
+//         target.position = (spawn.position + (2));
+//         target.rotation = spawn.rotation;
+//        }
+//        
+//    }
+}
