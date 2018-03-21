75d7e6807ef0b396972a1890a4b3dd52c64a6e0d
Mark Haynie <mhaynie2@uic.edu>
2018-03-20 23:12:40 -0500

    added patrol scene
    added test scene for basic ai

---
+++ the Barge, Atet/Assets/scripts/Follow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent agent;
+    public Transform target;
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(target.position.y < 3)
+        {
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+            agent.SetDestination(new Vector3(0, 0, 0));
+        }        
+    }
+}
