2ebf7e94f0c16423fc3ee81aa8f6f8815eb6577b
Mark Haynie <mhaynie2@uic.edu>
2018-04-04 04:18:29 -0500

    added movment to apep
    added apep animation
    added menu change to work with apep scene for tutorial

---
+++ the Barge, Atet/Assets/scripts/SimpleApep.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleApep : MonoBehaviour {
+
+    private Animator mAnimator;
+
+    public GameObject Apep;
+
+    private Vector3 lastLocation;
+
+    private bool mMove;
+
+	// Use this for initialization
+	void Start () {
+        mAnimator = GetComponent<Animator>();
+        lastLocation = Apep.transform.position;
+        mMove = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lastLocation != Apep.transform.position)
+        {
+            lastLocation = Apep.transform.position;
+            mMove = true;
+        }
+        else
+        {
+            mMove = false;
+        }
+
+        mAnimator.SetBool("Move", mMove);
+	}
+}
