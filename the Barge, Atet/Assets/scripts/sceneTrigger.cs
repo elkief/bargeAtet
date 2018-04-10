e8fb75e5e07ff19af3625f5bffcf8c4947b5219d
Em <emi@10-151-216-215.vlan912.client.wireless.uic.edu>
2018-04-10 16:55:29 -0500

    extra models and 1st hieroglyphic panel

---
+++ the Barge, Atet/Assets/scripts/sceneTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneTrigger : MonoBehaviour {
+    
+public void 
+    
+
+public void goToScene(string chosenScene){
+
+    SceneManager.LoadScene(chosenScene);
+}
+
+
+}
