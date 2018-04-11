9a13eeb4ebbfc46506dbb2c5c3a100e8d959bf64
Em <emi@10-151-216-215.vlan912.client.wireless.uic.edu>
2018-04-10 23:26:05 -0500

    materials edit

--- the Barge, Atet/Assets/scripts/sceneTrigger.cs
+++ the Barge, Atet/Assets/scripts/sceneTrigger.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class sceneTrigger : MonoBehaviour {
+<<<<<<< HEAD
 
+=======
+    
+    
+>>>>>>> e27a034e65ebd90ee4e0e101b46582785b10c21a
 
 public void goToScene(string chosenScene){
 
