d45c23fed3cecf930d667c4d38b10c0224018b0d
Mark Haynie <mhaynie2@uic.edu>
2018-04-11 02:29:55 -0500

    added text break
    added text break before main game

---
+++ the Barge, Atet/Assets/animations/Fade/FadeOutBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FadeOutBehaviour : MonoBehaviour {
+
+    public int index;
+    public string levelName;
+
+    public Image black;
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(Fading());   
+    }
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => black.color.a == 1);
+    }
+}
---
+++ the Barge, Atet/Assets/scripts/HideUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideUI : MonoBehaviour {
+
+    public bool isHidden;
+    public bool beingPressed;
+    public GameObject UIDisplay;
+
+	// Use this for initialization
+	void Start () {
+        isHidden = false;
+        beingPressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Tab))
+        {
+            if(!beingPressed)
+            {
+                if (isHidden)
+                {
+                    UIDisplay.SetActive(true);
+                    isHidden = false;
+                }
+                else
+                {
+                    UIDisplay.SetActive(false);
+                    isHidden = true;
+                }
+            }
+            beingPressed = true;
+        }
+        else
+        {
+
+            beingPressed = false;
+        }
+	}
+}
