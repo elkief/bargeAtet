6535ef3549f09bdb024cb8683df17ae9471cda43
Em <emi@10-151-216-215.vlan912.client.wireless.uic.edu>
2018-04-18 05:10:49 -0500

    push 1
    junkyard and audio script

---
+++ the Barge, Atet/Assets/scripts/audioOnCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioOnCollide : MonoBehaviour {
+
+	public AudioSource playSource;
+    
+    void Start(){
+        
+        playSource = GetComponent<AudioSource> ();
+        
+    }
+    
+   void OnCollisionEnter(Collision itHit){
+       
+       if(itHit.gameObject.tag == "sound"){
+      
+        playSource.Play();
+        }
+    } 
+}
+     
