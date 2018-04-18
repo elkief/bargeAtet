03adb3667d8d43dcd68def9753237490a60b5ae6
Em <emi@10-151-216-215.vlan912.client.wireless.uic.edu>
2018-04-17 21:23:12 -0500

    water shader construct
    simple waves and depth-based foam line along river

---
+++ the Barge, Atet/Assets/scripts/water&boat physics/DepthTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour {
+
+  private Camera cam;
+
+  void Start () {
+    cam = GetComponent<Camera>();
+    cam.depthTextureMode = DepthTextureMode.Depth;
+  }
+
+}
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//}
