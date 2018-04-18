b98d486b8adee0f5672df08209f8ab21d2cde0ab
Mark Haynie <mhaynie2@uic.edu>
2018-04-18 00:19:26 -0500

    fixed Main Text
    fixed scene between main text and main game so that it is all in one scene

---
+++ the Barge, Atet/Assets/scripts/ButtonsText.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsText : MonoBehaviour {
+
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+
+    private void Start()
+    {
+        text2.SetActive(false);
+        text3.SetActive(false);
+        text4.SetActive(false);
+    }
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void ClearBtn(GameObject clear)
+    {
+        Destroy(clear);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+    public void LoadText1(GameObject clearThis)
+    {
+        clearThis.SetActive(false);
+        text1.SetActive(true);
+    }
+
+    public void LoadText2(GameObject clearThis)
+    {
+        clearThis.SetActive(false);
+        text2.SetActive(true);
+    }
+
+    public void LoadText3(GameObject clearThis)
+    {
+        clearThis.SetActive(false);
+        text3.SetActive(true);
+    }
+
+    public void LoadText4(GameObject clearThis)
+    {
+        clearThis.SetActive(false);
+        text4.SetActive(true);
+    }
+}
