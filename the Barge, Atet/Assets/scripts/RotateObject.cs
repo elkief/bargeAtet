3fe4390d4d6864558bc736a6d93d3177098a2793
Mark Haynie <mhaynie2@uic.edu>
2018-04-18 02:35:39 -0500

    changed Junkyard3
    reworked lazer puzzle in scene and moved puzzles around

--- the Barge, Atet/Assets/scripts/RotateObject.cs
+++ the Barge, Atet/Assets/scripts/RotateObject.cs
@@ -6,28 +6,20 @@ public class RotateObject : MonoBehaviour {
 
     public GameObject rotateObject;
     public int forward = 0;
-    private Vector3 rotateRight = new Vector3(0, 1, 0);
-    private Vector3 rotateRightFast = new Vector3(0, 25, 0);
+    private Vector3 rotateRight = new Vector3(0, 10, 0);
+    private Vector3 rotateRightFast = new Vector3(0, 15, 0);
     private Vector3 rotateLeft = new Vector3(0, -10, 0);
-    private Vector3 rotateLeftFast = new Vector3(0, -25, 0);
+    private Vector3 rotateLeftFast = new Vector3(0, -15, 0);
 
     private void OnTriggerStay(Collider other)
     {
-        if(forward == 0)
-        {
-            rotateObject.transform.Rotate(rotateRight * Time.deltaTime);
-        }
-        else if (forward == 2)
+        if(Input.GetKey(KeyCode.V))
         {
             rotateObject.transform.Rotate(rotateRightFast * Time.deltaTime);
         }
-        else if (forward == 3)
+        else if (Input.GetKey(KeyCode.C))
         {
             rotateObject.transform.Rotate(rotateLeftFast * Time.deltaTime);
-        }
-        else
-        {
-            rotateObject.transform.Rotate(rotateLeft * Time.deltaTime);
         }
     }
 }
