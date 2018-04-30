7166d90c33bf2abc13adb906ecb0b5b9833cbb5f
p-welch <welch9@uic.edu>
2018-04-30 04:42:48 -0500

    Added sia abilities and 3 puzzles (pw puzzle sia scene). Also changed tomb and tweaked player physics

---
+++ the Barge, Atet/Assets/scripts/patrickAbilityScripts/clearSiaBlocks.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clearSiaBlocks : MonoBehaviour {
+
+    private AudioSource SiaAbilitySound = null;
+
+    GameObject[] visibles = null;
+    GameObject[] negVisibles = null;
+
+    public float range = 20;
+
+    public float negRange = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        if (visibles == null)
+        {
+            visibles = GameObject.FindGameObjectsWithTag("SiaObject");
+        }
+        if (negVisibles == null)
+        {
+            negVisibles = GameObject.FindGameObjectsWithTag("SiaObject1");
+
+        }
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "hu4")
+        {
+            if (visibles != null)
+            {
+                foreach (GameObject vis in visibles)
+                {
+                    if (vis != null)
+                    {
+                        if (range > Vector3.Distance(this.transform.position, vis.transform.position))
+                        {
+                            Destroy(vis);
+                        }
+                    }
+                }
+
+            }
+
+            if (negVisibles != null)
+            {
+                foreach (GameObject vis in negVisibles)
+                {
+                    if (vis != null)
+                    {
+                        if (negRange > Vector3.Distance(this.transform.position, vis.transform.position))
+                        {
+                            Destroy(vis);
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    // Update is called once per frame
+}
---
+++ the Barge, Atet/Assets/scripts/patrickAbilityScripts/patTriggerSia.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patTriggerSia : MonoBehaviour {
+
+    public GameObject SiaAudioSource;
+    private AudioSource SiaAbilitySound = null;
+
+    GameObject[] visibles = null;
+    GameObject[] negVisibles = null;
+
+    public float range = 5;
+
+    public float negRange = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (visibles == null) {
+            visibles = GameObject.FindGameObjectsWithTag("SiaObject");
+            foreach (GameObject vis in visibles)
+            {
+                //tan.transform.localScale = new Vector3(0, 0, 0);
+                Renderer rend = vis.GetComponent<Renderer>();
+                rend.enabled = false;
+            }
+        }
+        if (negVisibles == null)
+        {
+            negVisibles = GameObject.FindGameObjectsWithTag("SiaObject1");
+            foreach (GameObject vis in negVisibles)
+            {
+                Renderer rend = vis.GetComponent<Renderer>();
+                rend.enabled = true;
+            }
+
+        }
+
+        SiaAbilitySound = SiaAudioSource.GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (visibles != null)
+        {
+            foreach (GameObject vis in visibles)
+            {
+                if (vis != null)
+                {
+                    Renderer rend = vis.GetComponent<Renderer>();
+                    if (range > Vector3.Distance(this.transform.position, vis.transform.position))
+                    {
+                        rend.enabled = true;
+                    }
+                    else
+                    {
+                        rend.enabled = false;
+                    }
+                }
+            }
+
+        }
+
+        if (negVisibles != null)
+        {
+            foreach (GameObject vis in negVisibles)
+            {
+                if (vis != null)
+                {
+                    Renderer rend = vis.GetComponent<Renderer>();
+
+                    if (negRange > Vector3.Distance(this.transform.position, vis.transform.position))
+                    {
+                        rend.enabled = false;
+                    }
+                    else
+                    {
+                        rend.enabled = true;
+                    }
+                }
+            }
+
+        }
+
+
+    //GameObject.Find("HuCube").transform.localScale = new Vector3(0, 0, 0);
+
+        
+    }
+}
